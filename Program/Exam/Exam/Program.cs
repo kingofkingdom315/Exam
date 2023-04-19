b21780470138f05e1b6ad59277f750adfd86ce1a
King <63289769+kingofkingdom315@users.noreply.github.com>
2023-04-19 11:33:05 +0300

    закончил работу над программой

--- Program/Exam/Exam/Program.cs
+++ Program/Exam/Exam/Program.cs
@@ -16,11 +16,11 @@ namespace Exam
             public DateTime borndate;
         };
 
-        unsafe class PersonnelManagement
+        class PersonnelManagement
         {
             public Person[] person;
 
-            PersonnelManagement(Person[] buf, int n)
+            public PersonnelManagement(Person[] buf)
             {
                 try
                 {
@@ -42,7 +42,7 @@ namespace Exam
                     {
                         for (int j = 0; j < person.Length; j++)
                         {
-                            if (person[i].borndate < person[j].borndate)
+                            if (person[i].borndate > person[j].borndate)
                             {
                                 buf = person[i];
                                 person[i] = person[j];
@@ -50,7 +50,7 @@ namespace Exam
                             }
                             else if (person[i].borndate == person[j].borndate)
                             {
-                                if (person[i].famalyname.CompareTo(person[j].famalyname) < 0)
+                                if (person[i].famalyname.ToLower().CompareTo(person[j].famalyname.ToLower()) < 0)
                                 {
                                     buf = person[i];
                                     person[i] = person[j];
@@ -72,8 +72,6 @@ namespace Exam
             {
                 try
                 {
-                    File.Create(filepath);
-
                     String s = "";
 
                     for (int i = 0; i < person.Length; i++)
@@ -97,7 +95,75 @@ namespace Exam
 
         static void Main(string[] args)
         {
+            int n;
+            Person[] pers;
+            String path, s;
+            int cur = 1,cur1 = 1;
 
+            try
+            {
+                Console.WriteLine("Введите размер массива");
+                n = int.Parse(Console.ReadLine());
+                PersonnelManagement management;
+
+                pers = new Person[n];
+
+                for (int i = 0; i < n; i++)
+                {
+                    Console.WriteLine("Введите имя");
+                    pers[i].name = Console.ReadLine();
+                    Console.WriteLine("Введите фамилию");
+                    pers[i].famalyname = Console.ReadLine();
+                    Console.WriteLine("Введите дату рождения");
+                    pers[i].borndate = Convert.ToDateTime(Console.ReadLine());
+                }
+
+                management = new PersonnelManagement(pers);
+
+                management.Sort();
+
+                path = "Person.txt";
+
+                management.Save(path);
+
+                s = File.ReadAllText(path);
+                Console.WriteLine("№" + cur); cur++;
+                Console.Write("имя: "); cur1++;
+                foreach (Char c in s)
+                {
+                    if (c == ';' && cur <= n)
+                    {
+                        Console.Write("\n№" + cur); cur++;
+                    }
+                    else if (c == ':')
+                    {
+                        if (cur1 == 1)
+                        {
+                            Console.Write("\nимя: "); cur1++;
+                        }
+                        else if (cur1 == 2)
+                        {
+                            Console.Write("\nфамилия: "); cur1++;
+                        }
+                        else if (cur1 == 3)
+                        {
+                            Console.Write("\nдата рождения: "); cur1 = 1;
+                        }
+                    }
+                    else
+                    {
+                        Console.Write(c);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+            finally
+            {
+                Console.Read();
+            }
         }
     }
 }
